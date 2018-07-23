da9927de1194d9ea2f7fba5a0d040ee9605c81ec
syed.irfanullah <irfansyed.pg@gmail.com>
2018-07-23 18:40:57 +0500

    all changes

---
+++ app/src/main/java/com/irfansyed/VAS/VASMonitring/globale.java
@@ -0,0 +1,18 @@
+package com.irfansyed.VAS.VASMonitring;
+
+import java.security.PublicKey;
+
+/**
+ * Created by irfansyed on 12/20/2017.
+ */
+
+public class globale {
+
+
+    public static String db_pk = "";
+
+// hi globale
+
+    // hey
+
+}
