7f92cc00937cafa0d546d84a5db6a615e8b714ee
Wasim Abbas <wasim_40@yahoo.com>
2018-09-11 10:38:19 +0500

    Commit sajid dates

--- app/src/main/java/com/irfansyed/VAS/VASMonitring/A/A4301_A4315.java
+++ app/src/main/java/com/irfansyed/VAS/VASMonitring/A/A4301_A4315.java
@@ -13,11 +13,18 @@ import android.widget.Toast;
 
 import com.irfansyed.VAS.VASMonitring.R;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import data.LocalDataManager;
 import utils.ClearAllcontrol;
 import utils.Gothrough;
 
 public class A4301_A4315 extends AppCompatActivity implements RadioButton.OnCheckedChangeListener, View.OnClickListener {
+
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String DATE_PATTERN = "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)";
 
     //Declaration Region
     Button
@@ -429,6 +436,29 @@ public class A4301_A4315 extends AppCompatActivity implements RadioButton.OnChec
 
         if (validateField() == false) {
             Toast.makeText(this, "Required fields are missing", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        pattern = Pattern.compile(DATE_PATTERN);
+
+        if (!validate(ed_A4306_1.getText().toString().trim())) {
+
+            ed_A4306_1.setError("Kindly enter a valid date");
+            ed_A4306_1.requestFocus();
+            return;
+        }
+
+        if (!validate(ed_A4306_2.getText().toString().trim())) {
+
+            ed_A4306_2.setError("Kindly enter a valid date");
+            ed_A4306_2.requestFocus();
+            return;
+        }
+
+        if (!validate(ed_A4307.getText().toString().trim())) {
+
+            ed_A4307.setError("Kindly enter a valid date");
+            ed_A4307.requestFocus();
             return;
         }
 
@@ -1405,5 +1435,55 @@ public class A4301_A4315 extends AppCompatActivity implements RadioButton.OnChec
         }
 
         return Gothrough.IamHiden(ll_A4315) != false;
+    }
+
+
+    public boolean validate(final String date) {
+
+        matcher = pattern.matcher(date);
+
+        if (date.equals("99/99/9999")) {
+
+            return true;
+
+        } else if (matcher.matches()) {
+
+            matcher.reset();
+
+            if (matcher.find()) {
+
+                String day = matcher.group(1);
+                String month = matcher.group(2);
+                int year = Integer.parseInt(matcher.group(3));
+
+                if (day.equals("31") &&
+                        (month.equals("4") || month.equals("6") || month.equals("9") ||
+                                month.equals("11") || month.equals("04") || month.equals("06") ||
+                                month.equals("09"))) {
+                    return false; // only 1,3,5,7,8,10,12 has 31 days
+                } else if (month.equals("2") || month.equals("02")) {
+                    //leap year
+                    if (year % 4 == 0) {
+                        if (day.equals("30") || day.equals("31")) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    } else {
+                        if (day.equals("29") || day.equals("30") || day.equals("31")) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
