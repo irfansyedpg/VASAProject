75049de62bbe21d2fbfd8facfa2fbf6568abe8b6
mwaqarfarooqui <waqarfarooqui.12@gmail.com>
2018-09-18 12:13:52 +0500

    Update strings file and some updates and little changes in db of que N2057

--- app/src/main/java/data/A/A4081_A4094.java
+++ app/src/main/java/data/A/A4081_A4094.java
@@ -11,31 +11,31 @@ public class A4081_A4094 {
         String query;
         query = "CREATE TABLE '" + TABLE_NAME + "' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'study_id' INTEGER,   " +
 
-                Global.A.A4081_A4094.A4081   + " TEXT," +
+                Global.A.A4081_A4094.A4081 + " TEXT," +
                 Global.A.A4081_A4094.A4082_u + " TEXT," +
                 Global.A.A4081_A4094.A4082_a + " TEXT," +
                 Global.A.A4081_A4094.A4082_b + " TEXT," +
                 Global.A.A4081_A4094.A4082_c + " TEXT," +
-                Global.A.A4081_A4094.A4083   + " TEXT," +
-                Global.A.A4081_A4094.A4084   + " TEXT," +
+                Global.A.A4081_A4094.A4083 + " TEXT," +
+                Global.A.A4081_A4094.A4084 + " TEXT," +
                 Global.A.A4081_A4094.A4085_u + " TEXT," +
                 Global.A.A4081_A4094.A4085_a + " TEXT," +
                 Global.A.A4081_A4094.A4085_b + " TEXT," +
-                Global.A.A4081_A4094.A4086   + " TEXT," +
+                Global.A.A4081_A4094.A4086 + " TEXT," +
                 Global.A.A4081_A4094.A4087_u + " TEXT," +
                 Global.A.A4081_A4094.A4087_a + " TEXT," +
                 Global.A.A4081_A4094.A4087_b + " TEXT," +
-                Global.A.A4081_A4094.A4088   + " TEXT," +
-                Global.A.A4081_A4094.A4089   + " TEXT," +
-                Global.A.A4081_A4094.A4090   + " TEXT," +
-                Global.A.A4081_A4094.A4091   + " TEXT," +
-                Global.A.A4081_A4094.A4092   + " TEXT," +
-                Global.A.A4081_A4094.A4093   + " TEXT," +
+                Global.A.A4081_A4094.A4088 + " TEXT," +
+                Global.A.A4081_A4094.A4089 + " TEXT," +
+                Global.A.A4081_A4094.A4090 + " TEXT," +
+                Global.A.A4081_A4094.A4091 + " TEXT," +
+                Global.A.A4081_A4094.A4092 + " TEXT," +
+                Global.A.A4081_A4094.A4093 + " TEXT," +
                 Global.A.A4081_A4094.A4094_u + " TEXT," +
                 Global.A.A4081_A4094.A4094_a + " TEXT," +
                 Global.A.A4081_A4094.A4094_b + " TEXT," +
                 Global.A.A4081_A4094.A4094_c + " TEXT," +
-                Global.A.A4081_A4094.STATUS  + " TEXT" +
+                Global.A.A4081_A4094.STATUS + " TEXT" +
 
                 ')';
 
--- app/src/main/java/data/A/A4206_A4207.java
+++ app/src/main/java/data/A/A4206_A4207.java
@@ -11,33 +11,33 @@ public class A4206_A4207 {
         String query;
         query = "CREATE TABLE '" + TABLE_NAME + "' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'study_id' INTEGER,   " +
 
-                Global.A.A4206_A4207.A4206      + " TEXT," +
-                Global.A.A4206_A4207.A4206_1    + " TEXT," +
-                Global.A.A4206_A4207.A4206_2    + " TEXT," +
-                Global.A.A4206_A4207.A4206_3    + " TEXT," +
-                Global.A.A4206_A4207.A4206_4    + " TEXT," +
-                Global.A.A4206_A4207.A4206_5    + " TEXT," +
-                Global.A.A4206_A4207.A4206_6    + " TEXT," +
-                Global.A.A4206_A4207.A4206_7    + " TEXT," +
-                Global.A.A4206_A4207.A4206_8    + " TEXT," +
-                Global.A.A4206_A4207.A4206_9    + " TEXT," +
-                Global.A.A4206_A4207.A4206_10   + " TEXT," +
-                Global.A.A4206_A4207.A4206_11   + " TEXT," +
-                Global.A.A4206_A4207.A4206_12   + " TEXT," +
-                Global.A.A4206_A4207.A4206_13   + " TEXT," +
-                Global.A.A4206_A4207.A4206_14   + " TEXT," +
-                Global.A.A4206_A4207.A4206_15   + " TEXT," +
-                Global.A.A4206_A4207.A4206_16   + " TEXT," +
-                Global.A.A4206_A4207.A4206_17   + " TEXT," +
-                Global.A.A4206_A4207.A4206_18   + " TEXT," +
-                Global.A.A4206_A4207.A4206_19   + " TEXT," +
-                Global.A.A4206_A4207.A4206_20   + " TEXT," +
-                Global.A.A4206_A4207.A4206_21   + " TEXT," +
-                Global.A.A4206_A4207.A4206_22   + " TEXT," +
+                Global.A.A4206_A4207.A4206 + " TEXT," +
+                Global.A.A4206_A4207.A4206_1 + " TEXT," +
+                Global.A.A4206_A4207.A4206_2 + " TEXT," +
+                Global.A.A4206_A4207.A4206_3 + " TEXT," +
+                Global.A.A4206_A4207.A4206_4 + " TEXT," +
+                Global.A.A4206_A4207.A4206_5 + " TEXT," +
+                Global.A.A4206_A4207.A4206_6 + " TEXT," +
+                Global.A.A4206_A4207.A4206_7 + " TEXT," +
+                Global.A.A4206_A4207.A4206_8 + " TEXT," +
+                Global.A.A4206_A4207.A4206_9 + " TEXT," +
+                Global.A.A4206_A4207.A4206_10 + " TEXT," +
+                Global.A.A4206_A4207.A4206_11 + " TEXT," +
+                Global.A.A4206_A4207.A4206_12 + " TEXT," +
+                Global.A.A4206_A4207.A4206_13 + " TEXT," +
+                Global.A.A4206_A4207.A4206_14 + " TEXT," +
+                Global.A.A4206_A4207.A4206_15 + " TEXT," +
+                Global.A.A4206_A4207.A4206_16 + " TEXT," +
+                Global.A.A4206_A4207.A4206_17 + " TEXT," +
+                Global.A.A4206_A4207.A4206_18 + " TEXT," +
+                Global.A.A4206_A4207.A4206_19 + " TEXT," +
+                Global.A.A4206_A4207.A4206_20 + " TEXT," +
+                Global.A.A4206_A4207.A4206_21 + " TEXT," +
+                Global.A.A4206_A4207.A4206_22 + " TEXT," +
                 Global.A.A4206_A4207.A4207_hour + " TEXT," +
-                Global.A.A4206_A4207.A4207_day  + " TEXT," +
-                Global.A.A4206_A4207.A4207      + " TEXT," +
-                Global.A.A4206_A4207.STATUS     + " TEXT" +
+                Global.A.A4206_A4207.A4207_day + " TEXT," +
+                Global.A.A4206_A4207.A4207 + " TEXT," +
+                Global.A.A4206_A4207.STATUS + " TEXT" +
 
                 ')';
 
