3c1c1df0a5f9356ce86ace0e8e69efd7beb467d2
Wasim Abbas <wasim_40@yahoo.com>
2018-10-01 17:35:47 +0500

    commit alot of changes

--- app/src/main/java/com/irfansyed/VAS/VASMonitring/Upload/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.irfansyed.VAS.VASMonitring.Upload;
-
-public class Constants {
-
-    //public static final String ROOT_URL = "http://192.168.1.12/sm/Welcome/collect_upload_data";
-
-    public static final String ROOT_URL = "http://10.199.22.23/sm/Welcome/collect_upload_data";
-    //public static final String URL_REGISTER = ROOT_URL+"web_services.php";
-}
