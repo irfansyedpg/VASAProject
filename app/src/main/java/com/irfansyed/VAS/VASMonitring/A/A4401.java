07b6ea0e894324e5180e4c00ca69374363d547f0
SAJID LATIF <indus1530@gmail.com>
2019-08-07 22:38:01 +0500

    A4351 02

---
+++ app/src/main/java/com/irfansyed/VAS/VASMonitring/A/A4401.java
@@ -0,0 +1,15 @@
+package com.irfansyed.VAS.VASMonitring.A;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.irfansyed.VAS.VASMonitring.R;
+
+public class A4401 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a4401);
+    }
+}
